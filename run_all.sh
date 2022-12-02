326faa32ff22cdfed4c089b4f2c59035fd76db7e
Hazel Koehler <acomputerdog@gmail.com>
2022-12-02 10:45:49 -0500

    Fix typo in run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo Running all solutions...
-dotnet build | /dev/null
+dotnet build > /dev/null
 dotnet run --no-build --project AdventOfCode Day01 Part1
 dotnet run --no-build --project AdventOfCode Day01 Part2
 dotnet run --no-build --project AdventOfCode Day02 Part1
