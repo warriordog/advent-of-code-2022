21f395dd202aed755c2a13844281948f632349b9
Hazel Koehler <acomputerdog@gmail.com>
2022-12-02 10:12:43 -0500

    Document ISolution

--- AdventOfCode/ISolution.cs
+++ AdventOfCode/ISolution.cs
@@ -1,6 +1,17 @@
 namespace AdventOfCode;
 
+/// <summary>
+/// An independently-executable solution to a day/part pair.
+/// Implementations must have a public no-args constructor.
+/// </summary>
 public interface ISolution
 {
+    /// <summary>
+    /// Execute the solution.
+    /// Implementations MAY be asynchronous.
+    /// </summary>
+    /// <param name="inputFile">Relative path to the input file to use</param>
+    /// <param name="solutionArgs">Additional command line arguments provided by the user</param>
+    /// <returns>Return a task to await, even if the solution is synchronous</returns>
     public Task Run(string inputFile, List<string> solutionArgs);
 }
