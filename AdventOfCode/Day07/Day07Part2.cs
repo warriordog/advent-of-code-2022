2aca53e6774bbfdbdd0703ca0f0dab863b0062f2
Hazel Koehler <acomputerdog@gmail.com>
2022-12-07 13:12:57 -0500

    Day 7

---
+++ AdventOfCode/Day07/Day07Part2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode.Day07;
+
+[Solution("Day07", "Part2")]
+[InputFile("input.txt")]
+[InputFile("test.txt", type: InputFileType.Test)]
+public class Day07Part2 : Day07
+{
+    private readonly ILogger<Day07Part2> _logger;
+    public Day07Part2(ILogger<Day07Part2> logger) => _logger = logger;
+
+    protected override void RunDay7(Directory rootDirectory)
+    {
+        const ulong MaxSpace = 70000000ul;
+        const ulong MinSpace = 30000000ul;
+
+        var usedSpace = rootDirectory.GetTotalSize();
+        var freeSpace = MaxSpace - usedSpace;
+        var targetSpace = MinSpace - freeSpace;
+        
+        _logger.LogDebug("Filesystem is using {used} / {total} bytes, leaving {free} bytes free.", usedSpace, MaxSpace, freeSpace);
+        _logger.LogDebug("We need to free at least {target} bytes to reach the minimum of {min} bytes free.", targetSpace, MinSpace);
+
+        var sizeToRemove = rootDirectory.AllDirectories
+            .Select(d => d.GetTotalSize())
+            .Where(size => size >= targetSpace)
+            .Min();
+        _logger.LogInformation("We need to delete a directory with [{size}] bytes to free up enough space.", sizeToRemove);
+    }
+}
--- README.md
+++ README.md
@@ -38,6 +38,7 @@ Feedback and questions are welcome!
 ### Solutions
 | Day                         | Part 1                                    | Part 2                                    | Name                                                           |
 |-----------------------------|-------------------------------------------|-------------------------------------------|----------------------------------------------------------------|
+| [Day07](AdventOfCode/Day07) | [Part1](AdventOfCode/Day07/Day07Part1.cs) | [Part2](AdventOfCode/Day07/Day07Part2.cs) | [No Space Left On Device](https://adventofcode.com/2022/day/7) |
 | [Day06](AdventOfCode/Day06) | [Part1](AdventOfCode/Day06/Day06Part1.cs) | [Part2](AdventOfCode/Day06/Day06Part2.cs) | [Tuning Trouble](https://adventofcode.com/2022/day/6)          |
 | [Day05](AdventOfCode/Day05) | [Part1](AdventOfCode/Day05/Day05Part1.cs) | [Part2](AdventOfCode/Day05/Day05Part2.cs) | [Supply Stacks](https://adventofcode.com/2022/day/5)           |
 | [Day04](AdventOfCode/Day04) | [Part1](AdventOfCode/Day04/Day04Part1.cs) | [Part2](AdventOfCode/Day04/Day04Part2.cs) | [Camp Cleanup](https://adventofcode.com/2022/day/4)            |
