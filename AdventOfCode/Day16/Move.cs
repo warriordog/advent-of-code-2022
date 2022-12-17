0c37ce4ce03dbb486f6a34c5d102d59260b7e427
Hazel Koehler <acomputerdog@gmail.com>
2022-12-16 22:23:16 -0500

    Day15 Part2 - document + remove unneeded code

--- AdventOfCode/Day16/Move.cs
+++ AdventOfCode/Day16/Move.cs
@@ -1,5 +1,6 @@
 ﻿namespace AdventOfCode.Day16;
 
+// Because writing "List<List<Move>>" gets REALLY old, REALLY fast.
 public class MoveSet : List<Move> {}
 
 public readonly record struct Move(int ValveIndex, bool OpenValve);
